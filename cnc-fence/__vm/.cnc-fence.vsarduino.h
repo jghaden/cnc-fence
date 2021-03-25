0cb3791fd32272e89235411312707b2f4bc29ca0
jghaden <jghaden912@gmail.com>
2021-03-25 09:36:48 -0500

    Renamed project to CNC-Fence.

--- cnc-fence/__vm/.cnc-fence.vsarduino.h
+++ cnc-fence/__vm/.cnc-fence.vsarduino.h
@@ -88,6 +88,6 @@ typedef void *__builtin_va_list;
 #define pgm_read_float(address_short) float()
 #define pgm_read_ptr(address_short)   short()
 
-#include "amish-fence.ino"
+#include "cnc-fence.ino"
 #endif
 #endif
